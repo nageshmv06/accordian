a10c1ce2111601c9db867be4c0ab01567f7caf04
nageshmv06 <nageshmv18@gmail.com>
2021-01-04 23:05:44 +0530

    Updates

---
+++ precache-manifest.ba581e2d43e6d5417177c3df385741c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebb223bef43baaf856fc663dd572e91",
+    "url": "/accordian/index.html"
+  },
+  {
+    "revision": "5f654ae91c0b72c52829",
+    "url": "/accordian/static/css/main.323f6155.chunk.css"
+  },
+  {
+    "revision": "c4b5aba120e2afb62074",
+    "url": "/accordian/static/js/2.36de4066.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordian/static/js/2.36de4066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f654ae91c0b72c52829",
+    "url": "/accordian/static/js/main.a544b2c3.chunk.js"
+  },
+  {
+    "revision": "1cfcda94bee147169c25",
+    "url": "/accordian/static/js/runtime-main.6e3e4ddb.js"
+  }
+]);
